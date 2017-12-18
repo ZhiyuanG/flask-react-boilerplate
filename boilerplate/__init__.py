5ba4d85f58d7b1008e4e8a0930e13afd50bb5ea9
ZhiyuanG <zyguo1230@126.com>
2017-12-18 19:07:43 +0800

    Add react frontend

--- boilerplate/__init__.py
+++ boilerplate/__init__.py
@@ -1,9 +1,14 @@
 """hello world for flask"""
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
     """ function for route / """
     return render_template("hello.html")
+
+@app.route('/bundle.js')
+def send_bundle():
+    #return url_for('static', filename='bundle.js')
+    return app.send_static_file('bundle.js')
---
+++ boilerplate/assets/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: path.join(__dirname, "../templates"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
